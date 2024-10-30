06356b06d5f43cdb2a78c12482e38609c9a1974a
DiegoRuidias <162191324+DiegoRuidias@users.noreply.github.com>
2024-10-29 19:55:36 -0500

    build

--- server.ts
+++ server.ts
@@ -3,14 +3,14 @@ import path from 'path';
 
 const app = express();
 
-// Cambia 'nombre-de-tu-proyecto' por el nombre exacto de tu proyecto en la carpeta dist
-app.use(express.static(path.join(__dirname, 'dist/aecollege-web')));
+// Cambia el nombre del proyecto en `dist/nombre-del-proyecto/browser`
+app.use(express.static(path.join(__dirname, '../dist/aecollege-web/browser')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/aecollege-web/browser/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/aecollege-web/browser/index.html'));
 });
 
-// Convierte `PORT` a un número, usando 10000 como valor por defecto
+// Configura el puerto desde `process.env.PORT` o usa un valor por defecto
 const port = parseInt(process.env["PORT"] || "10000", 10);
 app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor en ejecución en http://localhost:${port}`);
