71ad60ca50664c8f2e92f689f4ce47b3aba7f96b
DiegoRuidias <162191324+DiegoRuidias@users.noreply.github.com>
2024-10-29 23:44:00 -0500

    webconfig

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     importProvidersFrom(BrowserAnimationsModule),  // Importa BrowserAnimationsModule
-    ...appConfig.providers
+    ...appConfig.providers,
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ]
 }).catch(err => console.error(err));
 
