c33e43899723318d6e67c0f645e03748a5ea768a
DiegoRuidias <162191324+DiegoRuidias@users.noreply.github.com>
2024-11-26 04:15:38 -0500

    deployed

--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
 
   protected readonly http = inject(HttpClient)
 
-  protected readonly baseUrl: string = "http://localhost:8080/api"
+  protected readonly baseUrl: string = "https://aecollege-rest.onrender.com/api"
 
   constructor() { }
   
