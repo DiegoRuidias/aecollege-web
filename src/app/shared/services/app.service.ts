10f88ef9090afda1060e6b9f8681c5b860d6dfcd
DiegoRuidias <162191324+DiegoRuidias@users.noreply.github.com>
2024-12-05 19:50:15 -0500

    deploy ra

--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
 
   protected readonly http = inject(HttpClient)
 
-  protected readonly baseUrl: string = "https://lovely-acceptance-production.up.railway.app/api/"
+  protected readonly baseUrl: string = "https://lovely-acceptance-production.up.railway.app/api"
 
   constructor() { }
   
