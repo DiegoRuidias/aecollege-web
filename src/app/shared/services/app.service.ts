fbdbca6c01bb3141195fc403aef4068ad5474812
DiegoRuidias <162191324+DiegoRuidias@users.noreply.github.com>
2025-06-22 22:48:56 -0500

    nuevo back conectado

--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -8,8 +8,8 @@ export class AppService {
 
   protected readonly http = inject(HttpClient)
 
-  protected readonly baseUrl: string = "https://lovely-acceptance-production.up.railway.app/api"
+  protected readonly baseUrl: string = "https://aecollege-rest-production.up.railway.app/"
 
   constructor() { }
-  
+
 }
